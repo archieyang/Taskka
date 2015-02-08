8b2a2619568f4206049f45c15385945a4527c1e0
Archie <yangyxcn@gmail.com>
2015-02-08 17:34:00 +0800

    implement register, login and change password views
    Signed-off-by: Archie <yangyxcn@gmail.com>

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,39 +9,45 @@
  * Main module of the application.
  */
 angular
-  .module('taskkaApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'firebase'
-  ])
-  .constant('FIREBASE_URL', 'https://taskka.firebaseio.com/')
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-      })
-      .when('/post', {
-        templateUrl: 'views/post.html',
-        controller: 'TaskCtrl'
-      })
-      .when('/edit/:taskId', {
-        templateUrl: 'views/edit.html',
-        controller: 'TaskCtrl'
-      })
-      .when('/browse', {
-        templateUrl: 'views/browse.html',
-        controller: 'TaskCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('taskkaApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'firebase'
+    ])
+    .constant('FIREBASE_URL', 'https://taskka.firebaseio.com/')
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl',
+            })
+            .when('/post', {
+                templateUrl: 'views/post.html',
+                controller: 'TaskCtrl'
+            })
+            .when('/edit/:taskId', {
+                templateUrl: 'views/edit.html',
+                controller: 'TaskCtrl'
+            })
+            .when('/browse', {
+                templateUrl: 'views/browse.html',
+                controller: 'TaskCtrl'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl'
+            })
+            .when('/register', {
+                templateUrl: 'views/register.html'
+            })
+            .when('/login', {
+                templateUrl: 'views/login.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
