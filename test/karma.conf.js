9b6efcc89e544f12b9233689c4b2af65e2c72cc4
Archie <yangyxcn@gmail.com>
2015-02-18 13:58:30 +0800

    fix deploy bugs and deploy on firebase
    Signed-off-by: Archie <yangyxcn@gmail.com>

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -31,6 +31,8 @@ module.exports = function(config) {
       'bower_components/angularjs-toaster/toaster.js',
       'bower_components/moment/moment.js',
       'bower_components/angular-moment/angular-moment.js',
+      'bower_components/firebase/firebase.js',
+      'bower_components/angularfire/dist/angularfire.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
